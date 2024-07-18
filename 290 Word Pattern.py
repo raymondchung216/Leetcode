46ae0c156e88eda7f54de6b441e0a67205b45a8b
Raymond Chung <raymond.chung216@gmail.com>
2024-07-18 22:35:05 +1000

    290. Word Pattern python (easy)

---
+++ 290 Word Pattern.py
@@ -0,0 +1,19 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        arr = s.split()
+        if len(arr) != len(pattern):
+            print(len(arr), len(s) )
+            return False
+        tupleSet= set()
+        wordSet = set()
+        letterSet = set()
+        for letter,word in zip(pattern,arr) :
+            print(letter, word)
+            if letter not in letterSet and word not in wordSet:
+                letterSet.add(letter)
+                wordSet.add(word)
+                tupleSet.add((letter,word))
+            else:
+                if (letter,word) not in tupleSet:
+                    return False
+        return True
