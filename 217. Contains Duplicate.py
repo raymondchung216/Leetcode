229342854806ba5919626182e9e7eb888e1fb01e
Raymond Chung <raymond.chung216@gmail.com>
2024-07-25 19:42:10 +1000

    217. Contains Duplicate (easy)

---
+++ 217. Contains Duplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        uniq = set()
+        for num in nums:
+            if num in uniq:
+                return True
+            uniq.add(num)
+                
+        
